658dd657fdab79be62f54500934119e71f6ca41b
Vasile Midrigan <111rigan111@gmail.com>
2022-05-24 05:32:56 +0100

    Delete hardcoded components and Add props to Contact.js Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,22 @@ import Nav from "./Nav";
 import Hero from "./Hero";
 import Cards from "./Cards";
 
+import swimmer from "../img/swimmer.png";
+import bycicle from "../img/bycicle.png";
+
 export default function App() {
   return (
     <div className="app">
       <Nav />
       <Hero />
-      <Cards />
+      <Cards
+        img={swimmer}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   );
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,51 +5,22 @@ import wedding from "../img/wedding.png";
 import bycicle from "../img/bycicle.png";
 import star from "../img/star.png";
 
-export default function Cards() {
+export default function Cards(props) {
   return (
     <div className="cards">
       <div className="card">
         <div className="card__status">sold out</div>
         <div className="card__img">
-          <img src={swimmer} alt="a swimmer" />
+          <img src={props.img} alt="" />
         </div>
         <img src={star} className="card__rating__star" alt="a start icon" />
         <p className="card__rating">
-          5.0
-          <span>(6)•USA</span>
+          {props.rating}
+          <span>({props.reviewCount})•USA</span>
         </p>
-        <p className="card__subject">Life lessons with Katie Zaferes</p>
+        <p className="card__subject">{props.title}</p>
         <p className="card__price">
-          <span>From $136</span>/ person
-        </p>
-      </div>
-      <div className="card">
-        <div className="card__status">online</div>
-        <div className="card__img">
-          <img src={wedding} alt="a bride" />
-        </div>
-        <img src={star} className="card__rating__star" alt="a start icon" />
-        <p className="card__rating">
-          5.0
-          <span>(30)•USA</span>
-        </p>
-        <p className="card__subject">Learn wedding photography</p>
-        <p className="card__price">
-          <span>From $125</span>/ person
-        </p>
-      </div>
-      <div className="card">
-        <div className="card__img">
-          <img src={bycicle} alt="a bycicle" />
-        </div>
-        <img src={star} className="card__rating__star" alt="a start icon" />
-        <p className="card__rating">
-          4.8
-          <span>(2)•USA</span>
-        </p>
-        <p className="card__subject">Group Mountain Biking</p>
-        <p className="card__price">
-          <span>From $50</span>/ person
+          <span>From ${props.price}</span>/ person
         </p>
       </div>
     </div>
